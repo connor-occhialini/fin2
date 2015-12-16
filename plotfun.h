08fcd109e3530cd3df4ea7c7f869fcdabdfdff31
connor-occhialini <connor.occhialini@uconn.edu>
2015-12-15 19:14:27 -0500

    second commit

--- plotfun.h
+++ plotfun.h
@@ -5,7 +5,7 @@
 //takes arguments from array structures and outputs a GNUPLOT of the data with connecting lines.
 void pfun(double x[], double y[], double u[], double v[], int points)
 {
-    char * commandsForGnuplot[] = {"set title \"Integral Values as a Function of Distance\"","set logscale x 2", "set logscale y 10", "plot 'data.temp' u 1:2 w l, 'data.temp' u 1:3 w l, 'data.temp' u 1:4 w l"};
+    char * commandsForGnuplot[] = {"set terminal png","set output 'energy.png'","set title \"Integral Values as a Function of Distance\"","set logscale x 2", "set logscale y 10", "plot 'data.temp' u 1:2 title 'VEGAS' w l, 'data.temp' u 1:3 title 'Handmade Integrator' w l, 'data.temp' u 1:4 title 'Dipole Approximation' w l"};
     FILE * temp = fopen("data.temp", "w"); // write coordinates here.
     FILE * gnuplotPipe = popen ("gnuplot -persistent", "w");
     int i;
@@ -16,7 +16,7 @@ void pfun(double x[], double y[], double u[], double v[], int points)
 	}
 
  
-    for (i=0; i < 4; i++)
+    for (i=0; i < 6; i++)
     {
         fprintf(gnuplotPipe, "%s \n", commandsForGnuplot[i]); //Send commands to gnuplot one by one.
     }
